961da6e134023658c758835f1e74e36e7aefc92e
futinuti <amihailovp@gmail.com>
2023-03-19 10:12:54 +0200

    сброс и изменение пароля

--- src/core/urls.py
+++ src/core/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
 
 if settings.DEBUG:
